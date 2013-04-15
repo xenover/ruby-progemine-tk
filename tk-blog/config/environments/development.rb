1003641d7a822564500f39248ba887f7bd62b575
xenover <xenover@gmail.com>
2013-04-15 17:02:14 +0300

    Link into notification email. Conf into envs

--- tk-blog/config/environments/development.rb
+++ tk-blog/config/environments/development.rb
@@ -34,4 +34,7 @@ TkBlog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # email host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- tk-blog/config/environments/production.rb
+++ tk-blog/config/environments/production.rb
@@ -64,4 +64,7 @@ TkBlog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  #email host
+  config.action_mailer.default_url_options = { :host => '88.198.9.226:5300' }
 end
