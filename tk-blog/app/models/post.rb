ffff52a96d794383ded21991c98c2ab52346621f
xenover <xenover@gmail.com>
2013-04-15 17:24:15 +0300

    Add email validation

--- tk-blog/app/models/post.rb
+++ tk-blog/app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
 
   validates :name,  :presence => true
   validates :title, :presence => true, :length => {:minimum => 5}
+  validates_format_of :author_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :allow_blank => true
   
   has_many :comments, :dependent => :destroy
   has_many :tags
