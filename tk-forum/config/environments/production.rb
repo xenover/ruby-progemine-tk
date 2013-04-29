45a8e28cc46a2ed37f1089c7d03950bdf19bb3dc
xenover <xenover@gmail.com>
2013-04-29 16:43:03 +0300

    config changes

--- tk-forum/config/environments/production.rb
+++ tk-forum/config/environments/production.rb
@@ -9,7 +9,7 @@ TkForum::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -49,7 +49,7 @@ TkForum::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
