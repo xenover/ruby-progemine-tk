67c60a1670257d24bd8eaa79cf7a6a693d100a89
xenover <xenover@gmail.com>
2013-04-29 16:34:19 +0300

    Initial base for forum project

---
+++ tk-forum/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TkForum::Application.config.secret_token = 'fef63e7fa6ffb0df7e61d8cb1e34fe344e284a1b8593b59a0ea1b4e1165e42e85e6b690c6cdbdf212649c3f73c57bc974ff0e538dcd1f5e4803196086e78f6c7'
